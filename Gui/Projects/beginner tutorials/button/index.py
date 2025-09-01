2640a3f3102a86603bf126a42d77b6f8f3ea09ef
stanly-sudo <alys.git24@gmail.com>
2025-09-01 22:00:54 +0800

    line 11-18: click function, line 20-21: gui button and command inserted with the click functio, line 23-24: button styling background, foreground, and text, line 27-28: button state active background/foreground, line 30-32: button image insert and image positioning, line 34: button state either Active or Disabled, line 37-39: label function inserted with the count function to display on window, styled and positioned, line 41: label 2 appearing after count function is triggered, line 43: button display and position.

--- Gui/Projects/beginner tutorials/button/index.py
+++ Gui/Projects/beginner tutorials/button/index.py
@@ -4,10 +4,43 @@ from tkinter import *
 #! Instantiate and Window Title
 window = Tk()
 window.geometry("420x420")
-window.title("Labels")
+window.title("button")
 
 #* code starts below here:
 
+count = 0
+def click():
+    #print("Hello world")
+    global count
+    count+=1
+    #print(count)
+    label.config(text=count)
+    label2.pack()
+
+button = Button(window, text='Click me!!!')
+button.config(command=click) #* when writing a button command make a function and add it here exluding the parenthesis of the function passed to the Button function.
+                             #* performs callback of a function
+button.config(font=('Ink Free', 50, 'bold'))
+button.config(bg='#e85313')
+button.config(fg='#e8cf13')
+
+button.config(activebackground='#FF0000') #* changing active state of the button background
+button.config(activeforeground='#e8cf13') #* changing active state of the button foreground
+
+image = PhotoImage(file='./Gui/Projects/beginner tutorials/button/logo.png')
+button.config(image=image)
+button.config(compound='top') #* (top/bottom/right/left)
+
+#button.config(state=ACTIVE) #* (state=ATIVE/DISABLED)
+
+#display count on window
+label = Label(window, text=count)
+label.config(font=('Monospace', 50))
+label.pack()
+
+label2 = Label(window, image=image)
+
+button.pack()
 
 #! Display
 window.mainloop()
