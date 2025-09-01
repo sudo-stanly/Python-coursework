5e250083b000bc459fe4be9b714a1b118c5e7dea
stanly-sudo <alys.git24@gmail.com>
2025-09-01 22:58:36 +0800

    line 11-13: submit function that prints a username from an entry of the user with the get() function, the submit function is triggered by the submit button in line 22. line 15-16: delete function that deletes the entire text from the entry, the delete function is triggered by the delete button in line 26. line 18-19: backspace function this function deletes the last character from an entry, the backspace function is triggered by the backspace button in line 30. line 34: entry an input on the window, line 35-37: styling of the entry, line 38: default text inside the entry, line 39: state of the entry, line 40: width of the entry, line 41: hashing technique for an entry like passwords etc. , line 43: display of entry.

--- Gui/Projects/beginner tutorials/inputs/index.py
+++ Gui/Projects/beginner tutorials/inputs/index.py
@@ -3,11 +3,44 @@ from tkinter import *
 
 #! Instantiate and Window Title
 window = Tk()
-window.geometry("420x420")
+#window.geometry("420x420")
 window.title("Title")
 
 #* code starts below here:
 
+def submit():
+    username = entry.get() #* this retrieves the text we entered and stores it within the variable username.
+    print("Hello ",username)
+    
+def delete():
+    entry.delete(0, END) #* deletes the line of text
+    
+def backspace():
+    entry.delete(len(entry.get())-1, END) #* deletes last character
+
+#* submit button
+submit = Button(window, text='submit', command=submit)
+submit.pack(side = RIGHT) #* places button to the right
+
+#* delete button
+delete = Button(window, text='delete', command=delete)
+delete.pack(side = RIGHT) #* places button to the right
+
+#* backspace button
+backspace = Button(window, text='backspace', command=backspace)
+backspace.pack(side = LEFT) #* places button to the right
+
+# entry widget = textbox that acepts a single line of user input
+entry = Entry()                          #* input
+entry.config(font=('Sans serif', 50))
+entry.config(bg='#000000')
+entry.config(fg='#00ff00')              
+#entry.insert(0,'Enter something')        #* default text
+#entry.config(state=DISABLED)             #* state ACTIVE/DISABLED
+entry.config(width=10)                    #* size of the entry box
+#entry.config(show='*')                    #* this will hash or hide what you input on the entry (does not have to be an asterisk)
+
+entry.pack()                              #* display entry
 
 #! Display
 window.mainloop()
