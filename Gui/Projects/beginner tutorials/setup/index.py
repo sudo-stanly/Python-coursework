4d6a7f969f11e65f835d9d8ef5044983f10d9fcd
stanly-sudo <alys.git24@gmail.com>
2025-09-01 17:21:21 +0800

    line 10: window = Tk()

--- Gui/Projects/beginner tutorials/setup/index.py
+++ Gui/Projects/beginner tutorials/setup/index.py
@@ -3,3 +3,8 @@ from tkinter import *
 
 #* widgets = GUI elements: buttons, textboxes, labels, images
 #* windows = serves as a container to hold or contain widgets
+
+
+# creating and customizing our own window.
+
+window = Tk() #insantiate an instance of a window for us
