a5ea2a17936a3c758becbf0da591ead2d929d60f
stanly-sudo <alys.git24@gmail.com>
2025-09-01 17:36:47 +0800

    line 18-23: changing title of the window and changing window icon image

--- Gui/Projects/beginner tutorials/setup/index.py
+++ Gui/Projects/beginner tutorials/setup/index.py
@@ -15,5 +15,13 @@ window.geometry("420x450") #format (width x height)
 #* disable resizing (fixed size window)
 #window.resizable(False, False)  # (width, height)
 
+#* change title and icon of window
+window.title("First Gui Program") #change title
+
+#* convert image to photo image
+icon = PhotoImage(file='./Gui/Projects/beginner tutorials/setup/logo.png') # converting image to photo image
+window.iconphoto(True, icon)
+
+
 #* Display
 window.mainloop() #this will place window on computer screen, will also listen for events 
