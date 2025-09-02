4db5c9f92076fca32c57bf51530f62e128ba5ec5
stanly-sudo <alys.git24@gmail.com>
2025-09-02 08:26:59 +0800

    line 13-16: frame function, line 14 Instantiation of a frame. line 19-22: buttons inside of the frame.

--- Gui/Projects/beginner tutorials/frames/index.py
+++ Gui/Projects/beginner tutorials/frames/index.py
@@ -8,6 +8,20 @@ window.title("Title")
 
 #* code starts below here:
 
+# frame = a rectangular container to group and hold widgets.
+
+# create frame
+frame = Frame(window, bg='pink', bd=5, relief=SUNKEN)
+#frame.pack(side=BOTTOM) # display frame
+# frame.place(x=0, y=0)
+
+# replace window as frame
+Button(frame, text='w', font=('Consolas', 25), width=3).pack(side=TOP) # shortcut
+Button(frame, text='a', font=('Consolas', 25), width=3).pack(side=LEFT)
+Button(frame, text='s', font=('Consolas', 25), width=3).pack(side=LEFT)
+Button(frame, text='d', font=('Consolas', 25), width=3).pack(side=LEFT)
+
+
 
 #! Display
 window.mainloop()
