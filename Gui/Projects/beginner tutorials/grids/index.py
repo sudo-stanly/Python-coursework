ca742206737fb996b80c7f4f14a53448bd5f1e97
stanly-sudo <alys.git24@gmail.com>
2025-09-02 08:47:23 +0800

    tkinter grids

---
+++ Gui/Projects/beginner tutorials/grids/index.py
@@ -0,0 +1,13 @@
+import tkinter
+from tkinter import *
+
+#! Instantiate and Window Title
+window = Tk()
+window.geometry("420x420")
+window.title("Title")
+
+#* code starts below here:
+
+
+#! Display
+window.mainloop()
