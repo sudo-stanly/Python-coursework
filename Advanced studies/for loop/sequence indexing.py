fc45ea2a57c5bc18a248435399072e723f5786ca
stanly-sudo <alys.git24@gmail.com>
2025-09-09 01:14:18 +0800

    line 7-11: basic element indexing. line 14-20: indexing with range and length directly to control element indexing.

--- Advanced studies/for loop/sequence indexing.py
+++ Advanced studies/for loop/sequence indexing.py
@@ -4,3 +4,17 @@
     ! sequence indexing
 
 """
+
+myList = ["apple", "orange", "banana"]
+# for x in myList:
+#     print(x)
+#* this prints all the elements
+
+
+#! using range() and len directly
+for x in range(len(myList)):
+    print(myList[x])
+    
+    if myList[x] == "orange":
+        print("Hello world")
+        break
