ef403f888064b33b10cfc5d104d5a087026d7c7b
stanly-sudo <alys.git24@gmail.com>
2025-09-09 01:22:51 +0800

    line 14-20: commented. line 22-31: indexing a dictionary.

--- Advanced studies/for loop/sequence indexing.py
+++ Advanced studies/for loop/sequence indexing.py
@@ -12,9 +12,20 @@ myList = ["apple", "orange", "banana"]
 
 
 #! using range() and len directly
-for x in range(len(myList)):
-    print(myList[x])
+# for x in range(len(myList)):
+#     print(myList[x])
     
-    if myList[x] == "orange":
-        print("Hello world")
-        break
+#     if myList[x] == "orange":
+#         print("Hello world")
+#         break
+
+"""
+
+    * hmmm.. dictionary-
+
+"""
+
+myObj = { "name" : "John Doe", "age" : 23 }
+for key, value in myObj.items():
+    # print(key, value)
+    print(f"{key} : {value}")
