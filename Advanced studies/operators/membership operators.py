aa1c32655639dbeb333748c783961fb60a9a45d6
stanly-sudo <alys.git24@gmail.com>
2025-09-13 16:28:51 +0800

    membership operators: example.

--- Advanced studies/operators/membership operators.py
+++ Advanced studies/operators/membership operators.py
@@ -1,5 +1,18 @@
 """
-
+ the
     ! python membership operators
 
 """
+
+#!         Operator                 Description                             Example
+#*            in                    Returns True if a sequence with         x in y
+#*                                  the specified value is present
+#*                                  in the onject.
+x = ["apple", "banana"]
+print("banana" in x) #* returns True because a sequence with the value "banana" is in the list
+
+
+#*          not in 	                Returns True if a sequence with the     x not in y
+#*                                  specified value is not present in
+#*                                  the object.
+print("pineapple" not in x) #* returns True because a sequence with the value "pineapple" is not in the list.
