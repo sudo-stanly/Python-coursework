8576447fb6a020d1377766d2ccd875faf1088c79
stanly-sudo <alys.git24@gmail.com>
2025-09-13 17:47:50 +0800

    line 1-5: introduction to modifying strings. line 7-10: upper() method, this returns the string in upper case. line 13-15: lower() method, this returns the string in lower case. line 18-20: strip() method, this removes the whitespace from the beginning or the end of a string. line 24-25: replace() method, this replaces a specific string with another string. line 29-31: split() method, this splits the string into substrings if it finds isntances of the separator.

--- Advanced studies/strings/modify strings.py
+++ Advanced studies/strings/modify strings.py
@@ -3,3 +3,29 @@
     ! modify strings
 
 """
+
+#! upper case:
+#* the upper() method returns the string in upper case.
+a = "Hello, World!"
+# print(a.upper()) #* prints "HELLO, WORLD!"
+
+
+#! lower case:
+#* the lower() method returns the string in lower case.
+# print(a.lower()) #* prints "hello, world!"
+
+
+#! remove whitespace, strip():
+#* the strip() method removes any whitespace from the beginning or the end.
+b = " Hello, World! "
+# print(f"from ' Hello, World! ' to '{b.strip()}'")
+
+
+#! replace string, replace():
+#* the replace() method replaces a string with another string.
+# print(b.replace("H", "J")) #* prints Jello, World!
+
+
+#! split string, split():
+#* the split() method splits the string into substrings if it finds isntances of the separator.
+# print(b.split(",")) #* returns ['Hello', ' World!']
