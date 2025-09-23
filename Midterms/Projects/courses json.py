25577bab5a8a9fc493a41965bd3a967bc1aeef3d
stanly-sudo <alys.git24@gmail.com>
2025-09-24 07:29:47 +0800

    midterms project: (in progress) learning how to unpack and acces a json with for loop and appending specific subjects and the subject grades which is 0 by default to a list, since this project is getting more and more complex and is expanding because of adding courses, subjects, which the main point was to simply grade the student but then again i want to learn more throughout this project.

--- Midterms/Projects/courses json.py
+++ Midterms/Projects/courses json.py
@@ -1,111 +1,4 @@
-
-#* -> testing access
-
-# subjects = [
-#     {
-#         "year_1_subjects" : {
-#             "1st_semester" : {
-#                 "programming" : {
-#                     "prelims" : 0,
-#                     "midterms" : 0,
-#                     "prefinals" : 0,
-#                     "finals" : 0
-#                 },
-#                 "introduction_to_computing" : {
-#                     "prelims" : 0,
-#                     "midterms" : 0,
-#                     "prefinals" : 0,
-#                     "finals" : 0
-#                 }
-#             }
-#         }
-#     }
-# ]
-# print(subjects[0]["year_1_subjects"]["1st_semester"])
-
-#! important!
-# for year_level, semesters in subjects[0].items():
-#     print(year_level)
-    
-#     for semester, subjects in semesters.items():
-#         print(semester)
-        
-#         for subjects, grades in subjects.items():
-#             print(subjects, end=" ")
-            
-#             for subject_grade, grade in grades.items():
-#                 print(grade, end=" ")
-
-
-#*
-
-
-# subjects = [
-#     {
-#         "year_1_subjects" : {
-#             "bsit" : 
-#             {
-#                 "1st_semester" : 
-#                 {
-#                     "programming" : {
-#                         "prelims" : 0,
-#                         "midterms" : 0,
-#                         "prefinals" : 0,
-#                         "finals" : 0
-#                     },
-#                     "introduction_to_computing" : {
-#                         "prelims" : 0,
-#                         "midterms" : 0,
-#                         "prefinals" : 0,
-#                         "finals" : 0
-#                     }
-#                 }
-#             },
-#             "bscomsci" :
-#             {
-#                 "1st_semester":
-#                 {
-#                     "no available subjects" : None
-#                 }
-#             }
-#         }
-#     },
-#     {
-#         "year_2_subjects" : {
-#             "bsit" : 
-#             {
-#                 "1st_semester" : 
-#                 {
-#                     "subject" : "no available subject."
-#                 }
-#             }
-#         }
-#     },
-#     {
-#         "year_3_subjects" : {
-#             "bsit" : 
-#             {
-#                 "1st_semester" : 
-#                 {
-#                     "subject" : "no available subject."
-#                 }
-#             }
-#         }
-#     },
-#     {
-#         "year_4_subjects" : {
-#             "bsit" : 
-#             {
-#                 "1st_semester" : 
-#                 {
-#                     "subject" : "no available subject."
-#                 }
-#             }
-#         }
-#     },
-# ]
-
-
+import re
 
 subjects = [
     {
@@ -136,6 +29,7 @@ while True:
     try:
         x = int(input("[1] Enter , [2] Exit: "))
 
+        year_level = 0
         if x != 2:
             
             print()
@@ -170,21 +64,65 @@ while True:
                 #         print(f"[{k}] {subj}")
             """
             
+            # instead of input, get input coming from student info. (instead of manual, it is auto filled)
             x = None
-            x = int(input("[#] Choose a year level: "))
+            x = 1 # example: 1st year
             year_chosen = subjects[x - 1]
         
             for year_level, courses in year_chosen.items():
                 print(f"[#] Courses available in {year_level}")
                 
-
                 course_list = list(courses.keys())
                 for i, course in enumerate(course_list, start=1):
                     print(f"[{i}] : {course}")
                 
+                y = 1 #example: bsit
+                chosen_course = course_list[y - 1]
+                
+                print(f"\n[#] Semesters in {chosen_course}:")
+                semesters = courses[chosen_course]
 
+                semester_list = list(semesters.keys())
+                for j, sem in enumerate(semester_list, start=1):
+                    print(f"[{j}] : {sem}")
+                    
+                z = 1 # example: 1st sem
+                chosen_semester = semester_list[z - 1]
+
+                print(f"\n[#] Subjects in {chosen_course} - {chosen_semester}: ")
+                subjects_in_sem = semesters[chosen_semester]
+                subject_list = list(subjects_in_sem.keys())
+                for k, subj in enumerate(subject_list, start=1):
+                    print(f"[{k}] {subj}")
+                    
             
+            # user can then choose a subject for student.
+            while True:
+                
+                user = int(input("Choose a subject: "))
+                for year_level, courses in year_chosen.items():
+                    
+                    course_list = list(courses.keys())
+                    y = 1
+                    chosen_course = course_list[y - 1]
+                    
+                    semesters = courses[chosen_course]
+                    semester_list = list(semesters.keys())
+                    z = 1
+                    chosen_semester = semester_list[z - 1]
             
+                    semester_subjects = semesters[chosen_semester]
+                    subject_list = list(semester_subjects.keys())
+
+                    subject_index = user
+                    chosen_subject = subject_list[user - 1]
+                    
+                    list_of_subjects_for_student.append(chosen_subject)
+                    list_of_grades_for_students.append(semester_subjects[chosen_subject])
+                    
+                    print(f"subjects {list_of_subjects_for_student}")
+                    print(f"grades {list_of_grades_for_students}")
+                
         elif x == 2:
             break
 
