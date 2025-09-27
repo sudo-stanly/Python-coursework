0e7a2705bb9ac3a2c73e52bd16a04131bc1720e6
stanly-sudo <alys.git24@gmail.com>
2025-09-28 07:15:09 +0800

    advanced studies: dictionaries

---
+++ Advanced studies/dictionaries/part 1.py
@@ -0,0 +1,13 @@
+# basic dictionary
+student = {
+    "name": "Linus",
+    "age": 19,
+    "course": "BSIT"
+}
+#accessing values
+print(student["name"])
+
+#adding new key/value
+student["year"] = 1
+print(student)
+
