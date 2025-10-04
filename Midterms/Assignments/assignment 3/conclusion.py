4b6717da231b471421ebcd664ac0f6cf94627f0c
stanly-sudo <alys.git24@gmail.com>
2025-10-05 07:00:22 +0800

    midterms assignment: (in progress) conclusion.

---
+++ Midterms/Assignments/assignment 3/conclusion.py
@@ -0,0 +1,5 @@
+
+#! CONCLUSION:
+# xml is just a mockup language, it has a tree of root(as a parent), child and subchild, it is also
+# self defined, and it has attributes. we can use xml to get http request to display a specific text
+# parsing method is in javascript but later will add how to parse using python.
